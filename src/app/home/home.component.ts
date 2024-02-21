fb0810a95d99c53116dbdad77022206b71fda696
felix <felix.renaidl@gmaio.com>
2024-02-21 20:45:34 +0700

    add chartjs

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -5,51 +5,128 @@ import { Router } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
 import { ConfigService } from 'src/app/service/config.service';
+import Chart from 'chart.js/auto';
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  chartJsBar = 'bar';
-  data = {
-    labels: ["January", "February",  ],
-    datasets: [
-      {
-        label: "My First dataset",
-        data: [65, 35,],
-        borderColor: '#36A2EB',
-        backgroundColor: '#9BD0F5',
-      }
-    ]
-  };
-  options = {
-    responsive: true,
-    maintainAspectRatio: false
-  };
-
+  // chartJsBar = 'bar';
+  // data = {
+  //   labels: ["loading"],
+  //   datasets: [
+  //     {
+  //       label: "Lenght of Service",
+  //       data: [1],
+  //       borderColor: '#36A2EB',
+  //       backgroundColor: '#9BD0F5',
+  //     }
+  //   ]
+  // }; 
+  // options = {
+  //   responsive: true,
+  //   maintainAspectRatio: false
+  // };
+  chart: any = [];
   dtOptionsReimbursement: ADTSettings = {};
   dtOptionsLoan: ADTSettings = {};
   dtOptionsReqestHoliday: ADTSettings = {};
 
-
+  employmentStatus: any = [];
+  employmentPosition: any = [];
+  gender: any = [];
+  lenghtOfService: any = [];
   constructor(
     config: NgbModalConfig,
     private modalService: NgbModal,
     private router: Router,
     private http: HttpClient,
     private configService: ConfigService,
-
-
   ) {
     config.backdrop = 'static';
     config.keyboard = false;
   }
   ngOnInit(): void {
-    this.httpGet();
+    this.datatables();
+    this.dashboard();
+  }
+  genderData : any = [];
+  colors : any = [];
+  dashboard() {
+    this.http.get<any>(environment.api + "home/index", {
+      headers: this.configService.headers(),
+    }).subscribe(
+      data => {
+        console.log(data);
+        this.employmentStatus = data['employmentStatus'];
+        this.employmentPosition = data['employmentPosition'];
+        this.gender = data['gender'];
+        this.genderData = data['genderData'];
+        this.lenghtOfService = data['lenghtOfService'];
+        this.colors = data['colors'];
+        this.initChartJs();
+        // this.data.datasets[0].data = data['lenghtOfService']['datasets']; 
+        // this.data.labels = data['lenghtOfService']['labels']; 
+
+      }
+    )
   }
 
-  httpGet() {
+  initChartJs() {
+    this.chart = new Chart('#LenghtofService', {
+      type: 'bar',
+      data: {
+        labels: this.lenghtOfService.labels,
+        datasets: [
+          {
+            label: 'Total Employment',
+            data: this.lenghtOfService.datasets,
+          },
+        ],
+      },
+      options: {
+        animation: {
+          duration: 0
+        },
+        scales: {
+          y: {
+            beginAtZero: true,
+          },
+          x: {
+            ticks: {
+              maxRotation: 90,
+              minRotation: 0
+            }
+          }
+        },
+        layout: {
+        
+          // padding: {
+          //   top: 550, // Atur tinggi yang diinginkan
+          // }
+        }
+      },
+
+    });
+
+    this.chart = new Chart('#GenderDiversity', {
+      type: 'doughnut',
+      data: {
+        labels: this.genderData.label,
+        datasets: [
+          {
+            label: 'Total Gender',
+            data: this.genderData.datasets,
+          },
+        ],
+      }, 
+    });
+
+  }
+
+  datatables() {
     console.log("httpGet");
     this.dtOptionsReimbursement = {
       ajax: {
@@ -197,9 +274,4 @@ export class HomeComponent implements OnInit {
     };
     this.router.navigate(['timeManagement/reports'], { queryParams: params });
   }
-
-
- 
-
-
 }
